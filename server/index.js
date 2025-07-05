3b00436e8e76973140f75f0e3d21996e0b6a6db8
Avineet__ <136901104+AvineetYadav@users.noreply.github.com>
2025-07-05 16:04:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "*",
+  origin: process.env.FRONTEND_URI || 'http://localhost:5173',
   credentials: true,
 }));
 
